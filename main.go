8fac42ad04ee501d9cc4875a3b2fb1b424a4d191
Sharath Kumar K <sharathkumar.k00@gmail.com>
2026-04-28 19:39:45 +0530

    Add update todo endpoint
    Add functionality for update todo
    Add restriction to avoid empty title in createTodo
    Add helper function to get todo by `id`
    Make `Title`, `Description` and `Done` field string and bool pointer
    respectively to distinguish client didn't provide data (field will be
    `nil`) vs client provided empty data (""/false)
    Add helper functions `stringPtr` and `boolPtr` to convert string to
    string pointer and boll pointer respectively.

--- main.go
+++ main.go
@@ -3,27 +3,46 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 var todos []Todo
+var Id int = 1
 
 type Todo struct {
 	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Done        bool      `json:"done"`
+	Title       *string   `json:"title"`
+	Description *string   `json:"description"`
+	Done        *bool     `json:"done"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func stringPtr(s string) *string { return &s }
+
+func boolPtr(b bool) *bool { return &b }
+
+func getTodoIdx(id int) (int, error) {
+	if id <= 0 {
+		return -1, fmt.Errorf("Bad id %d!", id)
+	}
+
+	for i := 1; i < len(todos); i++ {
+		if id == todos[i].ID {
+			return i, nil
+		}
+	}
+	return -1, fmt.Errorf("Todo with id %d not found!", id)
 }
 
 func getTodos(c *gin.Context) {
 	c.JSON(http.StatusOK, todos)
 }
 
-func createTodos(c *gin.Context) {
+func createTodo(c *gin.Context) {
 	var todo Todo
 	if err := c.ShouldBindJSON(&todo); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -31,31 +50,84 @@ func createTodos(c *gin.Context) {
 		})
 		return
 	}
-	todo.ID = todos[len(todos)-1].ID + 1
+
+	if *todo.Title == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Title required!",
+		})
+		return
+	}
+
+	todo.ID = Id
+	Id++
 	todo.CreatedAt = time.Now()
 	todo.UpdatedAt = time.Now()
 	todos = append(todos, todo)
 
-	c.JSON(http.StatusAccepted, todo)
+	c.JSON(http.StatusCreated, todo)
+}
+
+func updateTodo(c *gin.Context) {
+	var todo Todo
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid id",
+		})
+	}
+
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request",
+		})
+		return
+	}
+
+	todoIdx, err := getTodoIdx(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": fmt.Sprintf("Todo with id %d not found!", id),
+		})
+		return
+	}
+	oldTodo := &todos[todoIdx]
+
+	// Update only if the field is provided (not nil)
+	if todo.Title != nil {
+		oldTodo.Title = todo.Title
+	}
+	if todo.Description != nil {
+		oldTodo.Description = todo.Description
+	}
+	if todo.Done != nil {
+		oldTodo.Done = todo.Done
+	}
+	oldTodo.UpdatedAt = time.Now()
+
+	c.JSON(http.StatusAccepted, oldTodo)
 }
 
 func main() {
 	todo := Todo{
-		ID:          1,
-		Title:       "First todo",
-		Description: "This is my first test todo",
-		Done:        false,
+		ID:          Id,
+		Title:       stringPtr("First todo"),
+		Description: stringPtr("This is my first test todo"),
+		Done:        boolPtr(false),
 		CreatedAt:   time.Now(),
 		UpdatedAt:   time.Now(),
 	}
 	todos = append(todos, todo)
+	Id++
 
 	router := gin.Default()
 	fmt.Println("Server started...")
 
 	// Router
 	router.GET("/todos", getTodos)
-	router.POST("/todos/create", createTodos)
+	router.POST("/todos", createTodo)
+	router.PATCH("/todos/update/:id", updateTodo)
 
 	router.Run("localhost:8080")
 }
